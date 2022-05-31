c7e83563712ba5c1753d63531b560d8cb5dcf670
Felipe Souza <fgsouza93@gmail.com>
2022-05-30 23:20:44 -0300

    Add inter font

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -9,9 +9,16 @@ export default  createGlobalStyle`
     } 
     body {
         background: ${(props) => props.theme.background};
-        color: ${props => props.theme.text};        
+        color: ${props => props.theme.text}; 
+        /* font-family: 'Times New Roman', Times, serif       */
+        /* font-family: 'Courier New', Courier, monospace      */
+        font-family: 'Inter'
     }
     h1 {
         color: ${props => props.theme.titles};        
+    }
+    a {
+      color: inherit;
+      text-decoration: none;
     }
 `;
