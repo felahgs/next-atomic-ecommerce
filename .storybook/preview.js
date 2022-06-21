057ea6268ade66727da555fb482120f037bf921c
Felipe Souza <fgsouza93@gmail.com>
2022-06-20 21:36:28 -0300

    chore: add global theme to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,15 @@
 import { addDecorator } from '@storybook/react';
-import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from '../styles/theme';
 
-const themes = [lightTheme, darkTheme];
-addDecorator(withThemesProvider(themes), ThemeProvider);
+import GlobalStyles from 'styles/globalStyles';
+import { lightTheme } from 'styles/theme';
+
+addDecorator((storyFn) => (
+  <ThemeProvider theme={lightTheme}>
+    <GlobalStyles />
+    {storyFn()}
+  </ThemeProvider>
+));
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- src/components/atoms/Badge/Badge.stories.tsx
+++ src/components/atoms/Badge/Badge.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Button } from '@atoms/Button';
 
 import { Badge } from './Badge';
 
