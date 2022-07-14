c0771ee8454e05d329c7e119fb9abd0b9e61c2d6
Felipe Souza <fgsouza93@gmail.com>
2022-07-14 20:59:55 -0300

    test: ProductCard

--- src/components/organisms/ProductCard/ProductCard.test.tsx
+++ src/components/organisms/ProductCard/ProductCard.test.tsx
@@ -6,8 +6,19 @@ import { ProductCard } from './ProductCard';
 
 describe('Button', () => {
   it('should render the component', () => {
-    render(<ProductCard />);
-    expect(screen.getByRole('navigation')).toBeInTheDocument();
-    expect(screen.queryAllByRole('button')).toHaveLength(2);
+    render(
+      <ProductCard
+        image="/shoes.jpg"
+        description="A generic pair of shoes ready for any everyday run."
+        price="55"
+        watched={false}
+        shipping="Ships to anywhere"
+        rating={4.5}
+      />,
+    );
+
+    expect(screen.getByText(/generic pair of shoes/i)).toBeInTheDocument();
+    expect(screen.getByText(/Ships to anywhere/i)).toBeInTheDocument();
+    expect(screen.getByText('$55')).toBeInTheDocument();
   });
 });
